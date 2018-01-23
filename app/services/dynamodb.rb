8157c7535ab12b07767acc3edac811ca813028bb
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2018-01-23 20:58:29 +1300

    Modularise Dynamodb service

--- app/services/dynamodb.rb
+++ app/services/dynamodb.rb
@@ -1,13 +1,25 @@
-creds = CSV.read(APP_ROOT + 'aws-credentials.csv')[1]
-access_key = creds[0]
-secret_key = creds[1]
 
-dynamodb_client = Aws::DynamoDB::Client.new(
-  region: 'ap-southeast-2', # Syd-ers!
-  access_key_id: access_key,
-  secret_access_key: secret_key
-)
+module DynamoDB
+ def self.call
+    # Retrieve creds
+    creds = CSV.read(APP_ROOT + 'aws-credentials.csv')[1]
+    access_key = creds[0]
+    secret_key = creds[1]
 
+    # create dynamodb client
+    dynamodb_client = Aws::DynamoDB::Client.new(
+      region: 'ap-southeast-2', # Syd-ers!
+      access_key_id: access_key,
+      secret_access_key: secret_key
+    )
 
+    # set params for table scan
+    params = {
+      table_name: 'enviro_0001',
+      select: 'ALL_ATTRIBUTES'
+    }
 
-puts 1
+    # perform table scan
+    dynamodb_client.scan(params)
+ end
+end
