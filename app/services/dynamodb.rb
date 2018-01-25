d7ffc505188dc8e39047f39e027f5e807a55b09d
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2018-01-25 22:42:17 +1300

    Changes: * abstract core scan logic * add payload attribute * scan table upto three times for more data!!!

--- app/services/dynamodb.rb
+++ app/services/dynamodb.rb
@@ -4,28 +4,17 @@ class DynamoDB
   def initialize()
     @access_key = ENV['WINDY_ACCESS_KEY']
     @secret_key = ENV['WINDY_SECRET_KEY']
+    @payload = []
+    @scan_count = 0
   end
 
   def call
 
-    # create dynamodb client
-    dynamodb_client = Aws::DynamoDB::Client.new(
-      region: 'ap-southeast-2', # Syd-ers!
-      access_key_id: @access_key,
-      secret_access_key: @secret_key
-    )
+    scan
 
-    # set params for table scan
-    params = {
-      table_name: 'enviro_0001',
-      select: 'ALL_ATTRIBUTES',
-      limit: 1000
-    }
+    @payload.flatten!
 
-    # perform table scan
-    resp = dynamodb_client.scan(params)
-
-    sorted_items = resp.items.sort_by do |a|
+    sorted_items = @payload.sort_by do |a|
       a["Timestamp"]
     end
 
@@ -35,11 +24,50 @@ class DynamoDB
       item
     end
 
+    binding.pry
+
     sorted_items.map! do |item|
       [
         item["Timestamp"],
         item["WindSpeed"].to_i
       ]
     end
+  end
+
+  def scan(last_evaluated_key = '')
+
+    # set params for table scan
+    params = {
+      table_name: 'enviro_0001',
+      select: 'ALL_ATTRIBUTES'
+      # limit: 1000,
+    }
+
+    puts "Scan count: #{@scan_count}"
+    puts "Scanning"
+    # perform table scan
+    resp = client.scan(params)
+    @scan_count += 1
+
+    # add data to payload container
+    @payload.push(resp.items)
+
+    return @payload if @scan_count >= 3
+
+    if resp.last_evaluated_key
+      params["last_evaluated_key"] = last_evaluated_key
+      scan(resp.last_evaluated_key)
+    end
+
+    @payload
+  end
+
+  def client
+    # create dynamodb client
+    Aws::DynamoDB::Client.new(
+      region: 'ap-southeast-2', # Syd-ers!
+      access_key_id: @access_key,
+      secret_access_key: @secret_key
+    )
   end
 end
